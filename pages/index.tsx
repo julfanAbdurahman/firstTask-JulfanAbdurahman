ae1104fab28b371f550abc3de204aa0c5e96f5db
julfanAbdurahman <julfan2nd@gmail.com>
2024-02-21 16:14:46 +0700

    my-first-commit
    learn how to use useState and useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,48 @@
+import { log } from "console";
 import styles from "../styles.module.css";
+import React,{useState, useEffect} from "react";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSeacrTerm] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://efbdpjvnpulvomzjmpfz.supabase.co/functions/v1/rest-interview');
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setData(jsonData);
+    }
+    catch(error) {
+      console.error('Error fetching data ',error);
+    }
+  };
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
   return (
     <div className={styles.hello}>
-      <p>Hello World</p>
+      <p>Find Location</p>
+      <input 
+      type="text" 
+      placeholder="Enter Location Name"
+      value={searchTerm}
+      onChange={(e)=>setSeacrTerm(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Is Permanent</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
     </div>
   );
 };
